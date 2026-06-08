3a21de36d9e684bbae174af078a22b1e7de5a6ff
lhmontech <lhmonteiro.ti@gmail.com>
2026-06-08 10:57:39 -0300

    projeto da farmácia finalizado

--- JavaFX e BD/Farmacia_BD/src/main/java/org/example/controller/BalcaoController.java
+++ JavaFX e BD/Farmacia_BD/src/main/java/org/example/controller/BalcaoController.java
@@ -53,51 +53,23 @@ public class BalcaoController {
     private final Map<String, Image> imageCache = new HashMap<>();
 
     List<double[]> slots = List.of(
-            new double[]{152, 348, 48, 48},
-            new double[]{121, 363, 48, 48},
-            new double[]{233, 349, 48, 48},
-            new double[]{202, 364, 48, 48},
-            new double[]{350, 380, 48, 48},
-            new double[]{335, 405, 48, 48},
-            new double[]{400, 380, 48, 48},
-            new double[]{385, 405, 48, 48},
-            new double[]{496, 385, 48, 48},
-            new double[]{481, 410, 48, 48},
-            new double[]{546, 385, 48, 48},
-            new double[]{531, 410, 48, 48},
-            new double[]{471, 435, 48, 48},
-            new double[]{644, 386, 48, 48},
-            new double[]{629, 411, 48, 48},
-            new double[]{64, 500, 48, 48},
-            new double[]{49, 525, 48, 48},
-            new double[]{39, 550, 48, 48},
-            new double[]{143, 500, 48, 48},
-            new double[]{128, 525, 48, 48},
-            new double[]{118, 550, 48, 48},
-            new double[]{218, 500, 48, 48},
-            new double[]{193, 550, 48, 48},
-            new double[]{217, 501, 48, 48},
-            new double[]{202, 526, 48, 48},
-            new double[]{192, 551, 48, 48},
-            new double[]{292, 501, 48, 48},
-            new double[]{274, 526, 48, 48},
-            new double[]{259, 551, 48, 48},
-            new double[]{370, 501, 48, 48},
-            new double[]{355, 526, 48, 48},
-            new double[]{345, 551, 48, 48},
-            new double[]{445, 501, 48, 48},
-            new double[]{427, 526, 48, 48},
-            new double[]{412, 551, 48, 48},
-            new double[]{527, 502, 48, 48},
-            new double[]{512, 527, 48, 48},
-            new double[]{502, 552, 48, 48},
-            new double[]{602, 502, 48, 48},
-            new double[]{584, 527, 48, 48},
-            new double[]{569, 552, 48, 48},
-            new double[]{677, 526, 48, 48},
-            new double[]{667, 551, 48, 48},
-            new double[]{752, 524, 48, 48},
-            new double[]{737, 549, 48, 48}
+            new double[]{533, 56, 35, 50},
+            new double[]{572, 56, 35, 50},
+            new double[]{608, 56, 35, 50},
+            new double[]{646, 56, 35, 50},
+            new double[]{681, 56, 35, 50},
+            new double[]{716, 56, 35, 50},
+
+            new double[]{533, 137, 35, 50},
+            new double[]{568, 137, 35, 50},
+            new double[]{606, 137, 35, 50},
+            new double[]{641, 137, 35, 50},
+            new double[]{676, 137, 35, 50},
+            new double[]{711, 137, 35, 50},
+
+            new double[]{533, 215, 35, 50},
+            new double[]{572, 215, 35, 50},
+            new double[]{606, 215, 35, 50}
     );
 
     @FXML
@@ -174,7 +146,6 @@ public class BalcaoController {
             int quantidade = pegarQtdCli();
             boolean sucesso = farmaciaService.consumirProduto(nomeRemedio, quantidade);
             if (sucesso) {
-                animacaoTomar();
                 atualizarIndicadoresCli();
             } else {
                 AlertService.mostrar(anchorPane, "Operação não realizada. Verifique o estoque.", AlertService.Tipo.ERRO);
@@ -327,20 +298,6 @@ public class BalcaoController {
         qtdAmoxicilinaCli.setText(cesta.getOrDefault("Amoxicilina", 0) + "x");
 
         saldoCliente.setText("R$ " + farmaciaService.saldoCliente());
-    }
-
-    private void animacaoTomar() {
-        cliente.setVisible(false);
-        //clienteTomando.setVisible(true);
-
-        PauseTransition pausa = new PauseTransition(Duration.seconds(3));
-
-        pausa.setOnFinished(e -> {
-            //clienteTomando.setVisible(false);
-            cliente.setVisible(true);
-        });
-
-        pausa.play();
     }
 
     private void animacaoBalao(String texto) {
